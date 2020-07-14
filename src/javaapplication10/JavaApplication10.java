a47f105e24ceca90f8d1be6afb34d7f7c765b369
alvinchow97 <alvinchow97@gmail.com>
2020-07-14 17:20:49 +0800

    First initial

---
+++ src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,25 @@
+
+package javaapplication10;
+import static java.lang.System.out;
+import java.math.BigDecimal; 
+
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        BigDecimal bigTotal = new BigDecimal(0.40);
+        for( int n = 0; n < 100; n ++){
+            
+           bigTotal = bigTotal.add(BigDecimal.valueOf(0.4));
+        }
+        
+       bigTotal =bigTotal.setScale(3, BigDecimal.ROUND_HALF_UP);
+       out.println("total = " + bigTotal);
+       out.println("Testing");
+    }
+    
+}
